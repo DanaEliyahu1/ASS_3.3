cb6935a5337315a953563e808d68909ae49c287a
nimrodhaiat <nimnimch@gmail.com>
2019-06-07 19:40:34 +0300

    triedToDebug

--- pages/categories/categoryController.js
+++ pages/categories/categoryController.js
@@ -4,17 +4,15 @@
 
 
 angular.module("myApp").controller('categoryController', function($scope, $http) {
-  $http({
-    method : "GET",
-    url : "http://127.0.0.1:3000/view/getCategories" , 
-    headers :{
-        'x-auth-token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5pbXJvZCIsIm5hbWUiOiJuaW1yb2QiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTU5NzQzOTc2LCJleHAiOjE1NTk4MzAzNzZ9.hkqRxcFudzWysvggfHGAoCWflHW3khoKlh_H8TtZpcU",
-   
-        'Content-Type':'application/json'
-    }
-  }).then(function mySuccess(response) {
+  let myHeaders=new Headers();
+  myHeaders.append('Content-Type','application/json');
+  myHeaders.append('Accept','application/json');
+  myHeaders.append("x-auth-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5pbXJvZCIsIm5hbWUiOiJuaW1yb2QiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTU5ODE0NTY2LCJleHAiOjE1NTk5MDA5NjZ9.jb_wcz4IWrVdXlG0mecawmbM19R7OgZss_Y5rJN7fuU");
+  let options = new RequestOptions({ headers: headers });
+  $http.get('http://localhost:3000/view/getCategories',options)
+ .then(function mySuccess(response) {
       $scope.categories = response.data;
     }, function myError(response) {
       $scope.categories = response.statusText;
   });
-});
+}); 
